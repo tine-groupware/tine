82ef176d253c758d6902bc4f0ce8dc6b2cf5b304
Philipp Schüle <p.schuele@metaways.de>
2015-02-11 17:58:15 +0100

    conf(BE): add BE feature switch configuration
    * deactivates invoices, offers and order confirmations from BE
    ... switching the features off by default
    
    * disables Split view and context menu inBE
    
    * switches off some more features in BE
    see https://service.metaways.net/Ticket/Display.html?id=135728
    
    * updates feature switches for Egon 2016.11
    see #150165: [BE] Release 2016.11 Egon

---
+++ tine20/Calendar/config.inc.php
@@ -0,0 +1,11 @@
+<?php
+return array (
+    // this switches modules/features off in business edition
+    'features' => array(
+        'featureSplitView'                   => false,
+        'featureYearView'                    => false,
+        'featureColorBy'                     => true,
+        'featureExtendedEventContextActions' => true,
+        'featureRecurExcept'                 => false,
+    )
+);
