3994b58b0e75f8a57ecbc7eba0ad202458b6289c
Philipp Schüle <p.schuele@metaways.de>
2015-02-11 17:58:15 +0100

    deactivates invoices, offers and order confirmations from BE
    ... switching the features off by default
    
    Change-Id: I739dedef6ceaa2fed56e5c40df0dea6303072aaa
    Reviewed-on: http://gerrit.tine20.com/customers/1661
    Tested-by: Jenkins CI (http://ci.tine20.com/)
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

---
+++ tine20/Sales/config.inc.php
@@ -0,0 +1,9 @@
+<?php
+return array (
+    // this switches modules off in business edition
+    'features' => array(
+        'invoicesModule'             => false,
+        'offersModule'               => false,
+        'orderConfirmationsModule'   => false,
+    )
+);
