0d8b02070636eb8b2a853de47a7834b6d0ed2765
Philipp Schüle <p.schuele@metaways.de>
2015-07-15 18:11:05 +0200

    disables Split view and context menu inBE
    Change-Id: I3d5b12dd10c00afb22883082afffe444a23433d2
    Reviewed-on: http://gerrit.tine20.com/customers/2068
    Tested-by: Jenkins CI (http://ci.tine20.com/)
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

---
+++ tine20/Calendar/config.inc.php
@@ -0,0 +1,8 @@
+<?php
+return array (
+    // this switches modules off in business edition
+    'features' => array(
+        'featureSplitView'                   => false,
+        'featureExtendedEventContextActions' => false,
+    )
+);
