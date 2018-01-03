067cb520a447dd568ad73f8df429a2a04fc09030
Philipp Schüle <p.schuele@metaways.de>
2018-01-03 09:02:25 +0100

    replication docs and adb feature config
    us#1259 replication error resolve howto
    
    https://taiga.metaways.net/project/pschuele-erzbistum-hh/us/1259

---
+++ tine20/Addressbook/config.inc.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+    // this switches some Addressbook features on for Tine 2.0 BE
+    'features' => array(
+        Addressbook_Config::FEATURE_INDUSTRY => false,
+        Addressbook_Config::FEATURE_LIST_VIEW => true,
+        Addressbook_Config::FEATURE_RESOURCES => false,
+        Addressbook_Config::FEATURE_STRUCTUREPANEL => false,
+    )
+);
