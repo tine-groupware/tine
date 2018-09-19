e0143afb1ffb5af30a77e4fbb9c141af96ed81a0
Philipp Schüle <p.schuele@metaways.de>
2018-09-19 15:27:17 +0200

    config(Features): adjust BE feature switches
    Change-Id: Ic095a9d291c6a987a3f6be2ff2e9bd32e76bfda0
    Reviewed-on: http://gerrit.tine20.com/customers/10445
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Felamimail/config.inc.php
+++ tine20/Felamimail/config.inc.php
@@ -2,7 +2,7 @@
 return array (
     // this configures Felamimail features for Tine 2.0 BE
     'features' => array(
-        Felamimail_Config::FEATURE_TINE20_FLAG  => true,
+        Felamimail_Config::FEATURE_TINE20_FLAG   => true,
         Felamimail_Config::FEATURE_SHOW_REPLY_TO => false,
     ),
 );
