b04fca8b673c8262367c53edfcaa88ddbd53617e
Milan Mertens <m.mertens@metaways.de>
2018-11-12 12:20:18 +0100

    fix(Tinebase Preview Service) check and wait for license temp file
    Change-Id: Ic65000d0e869f0ca1453f0e28008dd17276365ad
    Reviewed-on: http://gerrit.tine20.com/customers/10893
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Tinebase/FileSystem/Preview/NetworkAdapter.php
+++ tine20/Tinebase/FileSystem/Preview/NetworkAdapter.php
@@ -32,6 +32,20 @@ class Tinebase_FileSystem_Preview_NetworkAdapter
     }
 
     /**
+     * @throws Tinebase_Exception_NotFound if license file was not copied
+     */
+    protected function _checkLicenseTempFile()
+    {
+        for ($i = 0; $i < 5; $i++) {
+            if (file_exists($this->_licensePath)) {
+                return;
+            }
+            sleep(1);
+        }
+        throw new Tinebase_Exception_NotFound("License temp file not found.");
+    }
+
+    /**
      * cleanup (license temp file)
      */
     public function __destruct()
@@ -44,15 +58,22 @@ class Tinebase_FileSystem_Preview_NetworkAdapter
     /**
      * @param null $config
      * @return Zend_Http_Client
+     * @throws Tinebase_Exception_NotFound license file not found
      */
     public function getHttpsClient($config = null)
     {
+        $this->_checkLicenseTempFile();
+
         $proxyConfig = Tinebase_Config::getInstance()->get(Tinebase_Config::INTERNET_PROXY);
         $curlOptions = array(
-            CURLOPT_SSLCERT=>$this->_licensePath,
-            CURLOPT_PROXY=>isset($proxyConfig['proxy_host']) ? $proxyConfig['proxy_host'] : '',
-            CURLOPT_PROXYUSERPWD=>isset($proxyConfig['proxy_user']) && isset($proxyConfig['proxy_pass']) ? $proxyConfig['proxy_user'].':'.$proxyConfig['proxy_pass'] : '',
-            CURLOPT_PROXYPORT=>isset($proxyConfig['proxy_port'])?$proxyConfig['proxy_port']:''
+            CURLOPT_SSLCERT=>
+                $this->_licensePath,
+            CURLOPT_PROXY=>
+                isset($proxyConfig['proxy_host']) ? $proxyConfig['proxy_host'] : '',
+            CURLOPT_PROXYUSERPWD=>
+                isset($proxyConfig['proxy_user']) && isset($proxyConfig['proxy_pass']) ? $proxyConfig['proxy_user'].':'.$proxyConfig['proxy_pass'] : '',
+            CURLOPT_PROXYPORT=>
+                isset($proxyConfig['proxy_port'])?$proxyConfig['proxy_port']:''
         );
         if (Tinebase_Config::getInstance()->{Tinebase_Config::FILESYSTEM}->{Tinebase_Config::FILESYSTEM_PREVIEW_SERVICE_VERIFY_SSL}) {
             $curlOptions[CURLOPT_CAPATH] = $this->_caPath;
