ebe2d8680b2c2ee53d238c21a8ff3839f3fb7bdc
Philipp Schüle <p.schuele@metaways.de>
2019-02-19 17:06:48 +0100

    conf(ADB/Tinebase): adopt BE config for new features
    Change-Id: I7b38edf39f2de9def4a089457b96c7ddc4cf3b86
    Reviewed-on: http://gerrit.tine20.com/customers/11885
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Addressbook/config.inc.php
+++ tine20/Addressbook/config.inc.php
@@ -6,5 +6,7 @@ return array (
         Addressbook_Config::FEATURE_LIST_VIEW => true,
         Addressbook_Config::FEATURE_RESOURCES => false,
         Addressbook_Config::FEATURE_STRUCTUREPANEL => false,
+        Addressbook_Config::FEATURE_CONTACT_EVENT_LIST => true,
+        Addressbook_Config::FEATURE_SHORT_NAME => false,
     )
 );
--- tine20/Tinebase/config.inc.php
+++ tine20/Tinebase/config.inc.php
@@ -8,6 +8,8 @@ return array (
         Tinebase_Config::FEATURE_SHOW_ACCOUNT_EMAIL    => false,
         Tinebase_Config::FEATURE_REMEMBER_POPUP_SIZE   => true,
         Tinebase_Config::FEATURE_SEARCH_PATH           => true,
+        Tinebase_Config::FEATURE_AUTODISCOVER          => true,
+        Tinebase_Config::FEATURE_AUTODISCOVER_MAILCONFIG => true,
     ),
     // branding / url config
     Tinebase_Config::BRANDING_WEBURL => 'https://www.tine20.com',
