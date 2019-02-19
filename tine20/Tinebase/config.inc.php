47ab94572df39444d0502c510ec193b50c5717ae
Philipp Schüle <p.schuele@metaways.de>
2019-02-19 17:06:48 +0100

    conf(ADB/Tinebase): adopt BE config for new features
    * conf(Container): remove obsolete feature FEATURE_CONTAINER_CUSTOM_SORT
    * conf(Addressbook/Mailinglist): deactivate mailinglist feature in BE

--- tine20/Tinebase/config.inc.php
+++ tine20/Tinebase/config.inc.php
@@ -3,15 +3,14 @@ return array (
     // this switches modules off in business edition
     'features' => array(
         Tinebase_Config::FEATURE_SHOW_ADVANCED_SEARCH  => false,
-        // TODO reactivate when this is working consistently
-        Tinebase_Config::FEATURE_CONTAINER_CUSTOM_SORT => false,
         Tinebase_Config::FEATURE_SHOW_ACCOUNT_EMAIL    => false,
         Tinebase_Config::FEATURE_REMEMBER_POPUP_SIZE   => true,
         Tinebase_Config::FEATURE_SEARCH_PATH           => true,
+        Tinebase_Config::FEATURE_AUTODISCOVER          => true,
+        Tinebase_Config::FEATURE_AUTODISCOVER_MAILCONFIG => true,
     ),
     // branding / url config
     Tinebase_Config::BRANDING_WEBURL => 'https://www.tine20.com',
-    // deactivate version check
-    // TODO activate when 2018.11.1 is released!
+    // activate in 2019.11 (when we have all distro (centos, ...) packages)
     Tinebase_Config::VERSION_CHECK => false,
 );
