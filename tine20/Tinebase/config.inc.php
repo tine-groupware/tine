499b632bcbf93e99ed68f22b1f03f478e1f7eefc
Philipp Schüle <p.schuele@metaways.de>
2019-02-26 13:47:26 +0100

    conf(Container): remove obsolete feature FEATURE_CONTAINER_CUSTOM_SORT
    Change-Id: Ib9d70f4caea08fabdc76135a70499abdf9078d9c
    Reviewed-on: http://gerrit.tine20.com/customers/11977
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
    Tested-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Tinebase/config.inc.php
+++ tine20/Tinebase/config.inc.php
@@ -3,8 +3,6 @@ return array (
     // this switches modules off in business edition
     'features' => array(
         Tinebase_Config::FEATURE_SHOW_ADVANCED_SEARCH  => false,
-        // TODO reactivate when this is working consistently
-        Tinebase_Config::FEATURE_CONTAINER_CUSTOM_SORT => false,
         Tinebase_Config::FEATURE_SHOW_ACCOUNT_EMAIL    => false,
         Tinebase_Config::FEATURE_REMEMBER_POPUP_SIZE   => true,
         Tinebase_Config::FEATURE_SEARCH_PATH           => true,
