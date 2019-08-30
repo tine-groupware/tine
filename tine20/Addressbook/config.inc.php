bdfb400362818a44648487674ba9c38e34b9e719
Philipp Schüle <p.schuele@metaways.de>
2019-08-30 11:15:44 +0200

    conf(Addressbook/Mailinglist): deactivate mailinglist feature in BE
    Change-Id: I673c4be8bbfd8b5d0eb23705e1b0b725cedae578
    Reviewed-on: http://gerrit.tine20.com/customers/13841
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Addressbook/config.inc.php
+++ tine20/Addressbook/config.inc.php
@@ -2,11 +2,12 @@
 return array (
     // this switches some Addressbook features on for Tine 2.0 BE
     'features' => array(
+        Addressbook_Config::FEATURE_CONTACT_EVENT_LIST => true,
         Addressbook_Config::FEATURE_INDUSTRY => false,
         Addressbook_Config::FEATURE_LIST_VIEW => true,
         Addressbook_Config::FEATURE_RESOURCES => false,
-        Addressbook_Config::FEATURE_STRUCTUREPANEL => false,
-        Addressbook_Config::FEATURE_CONTACT_EVENT_LIST => true,
+        Addressbook_Config::FEATURE_MAILINGLIST => false,
         Addressbook_Config::FEATURE_SHORT_NAME => false,
+        Addressbook_Config::FEATURE_STRUCTUREPANEL => false,
     )
 );
