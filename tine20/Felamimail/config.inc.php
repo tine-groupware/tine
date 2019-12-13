ed8f8ee857551d419842908fcd320868baad1904
Philipp Schüle <p.schuele@metaways.de>
2019-12-13 08:30:10 +0100

    conf(Felamimail): add new feature switches
    Change-Id: Ifc21d008ba004042f9491f1aa2ef4f24e61cef33
    Reviewed-on: http://gerrit.tine20.com/customers/15007
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Felamimail/config.inc.php
+++ tine20/Felamimail/config.inc.php
@@ -1,9 +1,10 @@
 <?php
 return array (
-    // this configures Felamimail features for Tine 2.0 BE
-    'features' => array(
+    'features' => [
+        Felamimail_Config::FEATURE_SYSTEM_ACCOUNT_AUTOCREATE_FOLDERS => true,
         Felamimail_Config::FEATURE_TINE20_FLAG   => true,
         Felamimail_Config::FEATURE_SHOW_REPLY_TO => false,
+        Felamimail_Config::FEATURE_ACCOUNT_MIGRATION => false,
         Felamimail_Config::FEATURE_AUTOSAVE_DRAFTS => false,
-    ),
+    ],
 );
