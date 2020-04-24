7ea4b3faefa1e44cc536558a051dc81acc2e5573
Philipp Schüle <p.schuele@metaways.de>
2020-04-24 10:03:25 +0200

    conf(HumanResources): activate FEATURE_WORKING_TIME_ACCOUNTING
    ... by default. it is checked again in license features.
    
    Change-Id: I602b54e1c80ce2e473b083f22b84b16a97e71a05
    Reviewed-on: http://gerrit.tine20.com/customers/16427
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

---
+++ tine20/HumanResources/config.inc.php
@@ -0,0 +1,8 @@
+<?php
+return array (
+    // this configures HumanResources features for Tine 2.0 BE
+    'features' => array(
+        HumanResources_Config::FEATURE_CALCULATE_DAILY_REPORTS => true,
+        HumanResources_Config::FEATURE_WORKING_TIME_ACCOUNTING => true,
+    ),
+);
