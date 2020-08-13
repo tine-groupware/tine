7040beda14bf01ef8d4319145a11fc604e57eb47
Philipp Schüle <p.schuele@metaways.de>
2020-08-13 17:40:48 +0200

    conf(Felamimail): update feature flags
    - add FEATURE_ACCOUNT_MOVE_NOTIFICATIONS (false)
    - enable FEATURE_AUTOSAVE_DRAFTS
    
    Change-Id: I28ecc697b9d1a3416e8b1814edc515838c4ef590
    Reviewed-on: http://gerrit.tine20.com/customers/17513
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Felamimail/config.inc.php
+++ tine20/Felamimail/config.inc.php
@@ -1,10 +1,11 @@
 <?php
 return array (
     'features' => [
+        Felamimail_Config::FEATURE_ACCOUNT_MIGRATION => false,
+        Felamimail_Config::FEATURE_ACCOUNT_MOVE_NOTIFICATIONS => false,
+        Felamimail_Config::FEATURE_AUTOSAVE_DRAFTS => true,
+        Felamimail_Config::FEATURE_SHOW_REPLY_TO => false,
         Felamimail_Config::FEATURE_SYSTEM_ACCOUNT_AUTOCREATE_FOLDERS => true,
         Felamimail_Config::FEATURE_TINE20_FLAG   => true,
-        Felamimail_Config::FEATURE_SHOW_REPLY_TO => false,
-        Felamimail_Config::FEATURE_ACCOUNT_MIGRATION => false,
-        Felamimail_Config::FEATURE_AUTOSAVE_DRAFTS => false,
     ],
 );
