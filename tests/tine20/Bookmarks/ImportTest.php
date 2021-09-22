0b2b863557a23afc633dcfc98c80bc95a052fd04
Philipp Schüle <p.schuele@metaways.de>
2021-09-22 11:38:40 +0200

    tests(Bookmarks/Import): fix import test
    ... examples/bookmarks.html file has moved
    
    also remove code duplication

--- tests/tine20/Bookmarks/ImportTest.php
+++ tests/tine20/Bookmarks/ImportTest.php
@@ -1,34 +1,35 @@
 <?php
+
 /**
  * Tine 2.0 - http://www.tine20.org
  * 
  * @package     Bookmarks
  * @subpackage  Import
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
- * @copyright   Copyright (c) 2020 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2020-2021 Metaways Infosystems GmbH (http://www.metaways.de)
  * @author      Timo Scholz <t.scholz@metaways.de>
  */
-
-
 
 /**
  * Test class for Bookmarks_ImportTest
  */
 class Bookmarks_ImportTest extends ImportTestCase
 {
-
-
-    public function testCliImport()
+    public function testCliImport($dryRun = false)
     {
         $countBeforeImport = Bookmarks_Controller_Bookmark::getInstance()->searchCount(Tinebase_Model_Filter_FilterGroup::getFilterForModel(Bookmarks_Model_Bookmark::class));
         $this->_deleteImportFile = false;
-        $this->_filename = __DIR__ . '/../../lib/Bookmarks/Import/examples/bookmarks.html';
+        $this->_filename = __DIR__ . '/../../../tine20/Bookmarks/Import/examples/bookmarks.html';
         $cli = new Bookmarks_Frontend_Cli();
         $opts = new Zend_Console_Getopt('abp:');
-        $opts->setArguments(array(
+        $args = array(
             'definition=bookmarks_import_html',
             $this->_filename
-        ));
+        );
+        if ($dryRun) {
+            $args[] = 'dryrun=1';
+        }
+        $opts->setArguments($args);
 
         ob_start();
         $cli->import($opts);
@@ -37,35 +38,12 @@ class Bookmarks_ImportTest extends ImportTestCase
         $countAfterImport = Bookmarks_Controller_Bookmark::getInstance()->searchCount(Tinebase_Model_Filter_FilterGroup::getFilterForModel(Bookmarks_Model_Bookmark::class));
 
         $this->assertStringContainsString('Imported 12 records', $out);
-        $this->assertEquals($countBeforeImport + 12, $countAfterImport);
-
+        $expectedCount = $dryRun ? $countBeforeImport: $countBeforeImport + 12;
+        $this->assertEquals($expectedCount, $countAfterImport);
     }
 
     public function testCliImportDryrun()
     {
-        $countBeforeImport = Bookmarks_Controller_Bookmark::getInstance()->searchCount(Tinebase_Model_Filter_FilterGroup::getFilterForModel(Bookmarks_Model_Bookmark::class));
-        $this->_deleteImportFile = false;
-        $this->_filename = __DIR__ . '/../../lib/Bookmarks/Import/examples/bookmarks.html';
-        $cli = new Bookmarks_Frontend_Cli();
-        $opts = new Zend_Console_Getopt('abp:');
-       
-        $opts->setArguments(array(
-            'definition=bookmarks_import_html',
-            'dryrun=1',
-            $this->_filename
-        ));
-        
-        ob_start();
-        $cli->import($opts);
-        $out = ob_get_clean();
-
-        $countAfterImport = Bookmarks_Controller_Bookmark::getInstance()->searchCount(Tinebase_Model_Filter_FilterGroup::getFilterForModel(Bookmarks_Model_Bookmark::class));
-
-        $this->assertStringContainsString('Imported 12 records', $out);
-        $this->assertEquals($countBeforeImport, $countAfterImport);
-
-
+        $this->testCliImport(true);
     }
-
-
 }
