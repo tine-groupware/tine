feb93bccbbe632964db23f6828f2687685a20a10
Philipp Schüle <p.schuele@metaways.de>
2021-12-06 16:08:40 +0100

    conf(Tinebase): activate autodiscover
    see # 200300: [saas] mx-record(s) für (*.)tine.cloud
     und einrichtung mail.tine.cloud

--- tine20/Tinebase/config.inc.php
+++ tine20/Tinebase/config.inc.php
@@ -1,6 +1,10 @@
 <?php
 return [
     // this switches some Tinebase features on for Saas
+    'features' => array(
+        Tinebase_Config::FEATURE_AUTODISCOVER => true,
+        Tinebase_Config::FEATURE_AUTODISCOVER_MAILCONFIG => true,
+    ),
     'accountDeletionEventConfiguration' => [
         Tinebase_Config::ACCOUNT_DELETION_DELETE_PERSONAL_CONTAINER => true,
         Tinebase_Config::ACCOUNT_DELETION_DELETE_PERSONAL_FOLDERS => true,
