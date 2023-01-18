8d82776540bf1ac4ee619c897316c50463e37b1e
Milan Mertens <m.mertens@metaways.de>
2023-01-18 15:12:26 +0100

    ci(docker): fix: webpackassets prefixed with auto

--- tine20/Tinebase/js/webpack.docker.js
+++ tine20/Tinebase/js/webpack.docker.js
@@ -5,6 +5,7 @@ var AssetsPlugin = require('assets-webpack-plugin');
 var assetsPluginInstance = new AssetsPlugin({
     path: '/out/tine20/Tinebase/js',
     keepInMemory: false,
+    removeFullPathAutoPrefix: true,
     filename: 'webpack-assets-FAT.json',
     prettyPrint: true
 });
