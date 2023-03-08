8276506d32661eeb190c548d6f954fb2a26bc8bf
ccheng-dev <c.cheng@metaways.de>
2023-03-08 09:51:13 +0100

    tweak(Felamimail/Update/16.3): update sieve script template

--- tine20/Felamimail/Setup/Update/16.php
+++ tine20/Felamimail/Setup/Update/16.php
@@ -16,6 +16,8 @@ class Felamimail_Setup_Update_16 extends Setup_Update_Abstract
     const RELEASE016_UPDATE000 = __CLASS__ . '::update000';
     const RELEASE016_UPDATE001 = __CLASS__ . '::update001';
     const RELEASE016_UPDATE002 = __CLASS__ . '::update002';
+    const RELEASE016_UPDATE003 = __CLASS__ . '::update003';
+
 
     static protected $_allUpdates = [
         self::PRIO_NORMAL_APP_STRUCTURE => [
@@ -32,6 +34,10 @@ class Felamimail_Setup_Update_16 extends Setup_Update_Abstract
             self::RELEASE016_UPDATE002          => [
                 self::CLASS_CONST                   => self::class,
                 self::FUNCTION_CONST                => 'update002',
+            ],
+            self::RELEASE016_UPDATE003          => [
+                self::CLASS_CONST                   => self::class,
+                self::FUNCTION_CONST                => 'update003',
             ],
         ],
     ];
@@ -52,5 +58,60 @@ class Felamimail_Setup_Update_16 extends Setup_Update_Abstract
         Tinebase_Core::getDb()->query('UPDATE ' . SQL_TABLE_PREFIX . 'preferences SET value = "message" WHERE name = "emlForward" and value = "0"');
         
         $this->addApplicationUpdate('Felamimail', '16.2', self::RELEASE016_UPDATE002);   
+    }
+    
+    public function update003()
+    {
+        if (! Tinebase_Core::isFilesystemAvailable()) {
+            if (Tinebase_Core::isLogLevel(Zend_Log::WARN)) Tinebase_Core::getLogger()->warn(__METHOD__ . '::' . __LINE__
+                . ' Skipping update of sieve notification template');
+        } else {
+            $basepath = Tinebase_FileSystem::getInstance()->getApplicationBasePath(
+                'Felamimail',
+                Tinebase_FileSystem::FOLDER_TYPE_SHARED
+            );
+
+            if (false === ($fh = Tinebase_FileSystem::getInstance()->fopen(
+                    $basepath . '/Email Notification Templates/defaultForwarding.sieve', 'w'))) {
+                if (Tinebase_Core::isLogLevel(Zend_Log::ERR)) {
+                    Tinebase_Core::getLogger()->err(__METHOD__ . '::' . __LINE__
+                        . ' Could not open defaultForwarding.sieve file');
+                }
+            } else {
+
+                fwrite($fh, <<<'sieveFile'
+require ["enotify", "variables", "copy", "body"];
+
+if header :contains "Return-Path" "<>" {
+    if body :raw :contains "X-Tine20-Type: Notification" {
+        notify :message "there was a notification bounce"
+              "mailto:ADMIN_BOUNCE_EMAIL";
+    }
+} elsif header :contains "X-Tine20-Type" "Notification" {
+    REDIRECT_EMAILS_SCRIPT
+} else {
+    if header :matches "Subject" "*" {
+        set "subject" "${1}";
+    }
+    if header :matches "From" "*" {
+        set "from" "${1}";
+    }
+    set :encodeurl "message" "TRANSLATE_SUBJECT${from}: ${subject}";
+
+    NOTIFY_EMAILS_SCRIPT
+}
+sieveFile
+                );
+
+                if (true !== Tinebase_FileSystem::getInstance()->fclose($fh)) {
+                    if (Tinebase_Core::isLogLevel(Zend_Log::ERR)) {
+                        Tinebase_Core::getLogger()->err(__METHOD__ . '::' . __LINE__
+                            . ' Could not close defaultForwarding.sieve file');
+                    }
+                }
+            }
+        }
+
+        $this->addApplicationUpdate('Felamimail', '16.3', self::RELEASE016_UPDATE003);
     }
 }
