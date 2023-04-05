9c00c9d0828e83fd400e0e111c3642fb90504f80
Philipp Schüle <p.schuele@metaways.de>
2023-04-05 17:20:22 +0200

    fix(Felamimail/Update/16.4): only update notification sieve if IMAP is managed

--- tine20/Felamimail/Setup/Update/16.php
+++ tine20/Felamimail/Setup/Update/16.php
@@ -123,7 +123,9 @@ sieveFile
 
     public function update004()
     {
-        Admin_Controller_EmailAccount::getInstance()->updateNotificationScripts();
+        if (Tinebase_EmailUser::manages(Tinebase_Config::IMAP)) {
+            Admin_Controller_EmailAccount::getInstance()->updateNotificationScripts();
+        }
         
         $this->addApplicationUpdate('Felamimail', '16.4', self::RELEASE016_UPDATE004);
     }
