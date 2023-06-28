2ad1ec6a62e9ddb83d4815b25c468faf882f0c71
Milan Mertens <m.mertens@metaways.de>
2023-06-28 11:46:48 +0000

    ci(release): release packages to new github

--- ci/gitlab-ci/lib/scripts/repo.sh
+++ ci/gitlab-ci/lib/scripts/repo.sh
@@ -34,7 +34,7 @@ repo_get_customer_for_branch () {
 
 repo_release_notes() {
     tag=$1
-    previous_tag=$2
+    previous_tag="$(git describe --abbrev=0 --tags HEAD~1 2> /dev/null || git fetch --unshallow --quiet && git describe --abbrev=0 --tags HEAD~1)" # if describe fails unshallow repo and try again
 
     echo '# Releasenotes'
     echo '# Changelog'
