d2550428113b0bc59cbd3faca2a736bd5607d1b9
Cornelius Weiß <c.weiss@metaways.de>
2023-06-30 14:34:40 +0200

    fix(Sales): update script 16.4 not working vor customers (non mcv2)

--- tine20/Sales/Setup/Update/16.php
+++ tine20/Sales/Setup/Update/16.php
@@ -18,6 +18,7 @@ class Sales_Setup_Update_16 extends Setup_Update_Abstract
     const RELEASE016_UPDATE002 = __CLASS__ . '::update002';
     const RELEASE016_UPDATE003 = __CLASS__ . '::update003';
     const RELEASE016_UPDATE004 = __CLASS__ . '::update004';
+    const RELEASE016_UPDATE005 = __CLASS__ . '::update005';
 
     static protected $_allUpdates = [
         self::PRIO_NORMAL_APP_STRUCTURE     => [
@@ -32,6 +33,10 @@ class Sales_Setup_Update_16 extends Setup_Update_Abstract
             self::RELEASE016_UPDATE004          => [
                 self::CLASS_CONST                   => self::class,
                 self::FUNCTION_CONST                => 'update004',
+            ],
+            self::RELEASE016_UPDATE005          => [
+                self::CLASS_CONST                   => self::class,
+                self::FUNCTION_CONST                => 'update005',
             ],
         ],
         self::PRIO_NORMAL_APP_UPDATE        => [
@@ -87,11 +92,28 @@ class Sales_Setup_Update_16 extends Setup_Update_Abstract
     public function update004()
     {
         Setup_SchemaTool::updateSchema([
-            Sales_Model_Customer::class,
             Sales_Model_DocumentPosition_Offer::class,
             Sales_Model_DocumentPosition_Order::class,
             Sales_Model_DocumentPosition_Invoice::class,
         ]);
         $this->addApplicationUpdate(Sales_Config::APP_NAME, '16.4', self::RELEASE016_UPDATE004);
+    }
+
+    public function update005()
+    {
+        Tinebase_TransactionManager::getInstance()->rollBack();
+        if ($this->getTableVersion('sales_customers') < 5) {
+            $declaration = new Setup_Backend_Schema_Field_Xml('
+                <field>
+                    <name>vat_procedure</name>
+                    <type>text</type>
+                    <length>40</length>
+                    <default>taxable</default>
+                </field>
+        ');
+            $this->_backend->addCol('sales_customers', $declaration, 10);
+            $this->setTableVersion('sales_customers', 5);
+        }
+        $this->addApplicationUpdate(Sales_Config::APP_NAME, '16.5', self::RELEASE016_UPDATE005);
     }
 }
