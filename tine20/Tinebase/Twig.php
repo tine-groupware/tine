e2fb996012c38e8b6aeeff5983602ae4547af545
Philipp Schüle <p.schuele@metaways.de>
2023-08-08 11:45:37 +0200

    tweak(Tinebase/Twig): prevent "undefined index" notice

--- tine20/Tinebase/Twig.php
+++ tine20/Tinebase/Twig.php
@@ -6,7 +6,7 @@
  * @subpackage  Twig
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Paul Mehrer <p.mehrer@metaways.de>
- * @copyright   Copyright (c) 2017-2018 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2017-2023 Metaways Infosystems GmbH (http://www.metaways.de)
  *
  */
 
@@ -221,7 +221,7 @@ class Tinebase_Twig
             $staticData[$key] = $data;
         }));
         $this->_twigEnvironment->addFunction(new Twig_SimpleFunction('getStaticData', function ($key) use(&$staticData) {
-            return $staticData[$key] ?: null;
+            return isset($staticData[$key]) ? ($staticData[$key] ?: null) : null;
         }));
 
         $this->_twigEnvironment->addFunction(new Twig_SimpleFunction('relationTranslateModel', function ($model) {
