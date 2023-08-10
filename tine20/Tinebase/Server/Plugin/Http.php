eaccff7715456eae33d654213c3b8638bda1fe76
Paul Mehrer <p.mehrer@metaways.de>
2023-08-10 16:55:35 +0200

    tweak(TB OpenId) remove legacy openID code

--- tine20/Tinebase/Server/Plugin/Http.php
+++ tine20/Tinebase/Server/Plugin/Http.php
@@ -26,20 +26,6 @@ class Tinebase_Server_Plugin_Http implements Tinebase_Server_Plugin_Interface
      */
     public static function getServer(\Laminas\Http\Request $request)
     {
-        /**************************** OpenID ****************************
-         * RewriteRule ^/users/(.*)                      /index.php?frontend=openid&username=$1 [L,QSA]
-         */
-        if (isset($_SERVER['HTTP_ACCEPT']) && stripos($_SERVER['HTTP_ACCEPT'], 'application/xrds+xml') !== FALSE) {
-            $_REQUEST['method'] = 'Tinebase.getXRDS';
-        } else if ((isset($_SERVER['REDIRECT_USERINFOPAGE']) && $_SERVER['REDIRECT_USERINFOPAGE'] == 'true') ||
-                   (isset($_REQUEST['frontend']) && $_REQUEST['frontend'] == 'openid')) {
-            $_REQUEST['method'] = 'Tinebase.userInfoPage';
-        }
-
-        if (!isset($_REQUEST['method']) && (isset($_REQUEST['openid_action']) || isset($_REQUEST['openid_assoc_handle'])) ) {
-            $_REQUEST['method'] = 'Tinebase.openId';
-        }
-
         return new Tinebase_Server_Http();
     }
 }
