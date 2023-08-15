786a9f22bb17aea7fa5d96331b2c6063c117cb52
Philipp Schüle <p.schuele@metaways.de>
2023-08-15 15:20:56 +0200

    tests(Tinebase/Server/Routing): removed borken assertions

--- tests/tine20/Tinebase/Server/RoutingTests.php
+++ tests/tine20/Tinebase/Server/RoutingTests.php
@@ -169,11 +169,5 @@ class Tinebase_Server_RoutingTests extends TestCase
             self::assertGreaterThanOrEqual(0, $decodedContent['totalEmailSharedAccounts']);
             self::assertGreaterThanOrEqual(0, $decodedContent['totalEmailMailingList']);
         }
-        
-        if (Tinebase_Application::getInstance()->isInstalled('SaasInstance')) {
-            self::assertGreaterThanOrEqual(2.2, $content['SaasInstance']['pricePerUser']);
-            self::assertGreaterThanOrEqual(0.5, $content['SaasInstance']['pricePerGigabyte']);
-            self::assertGreaterThanOrEqual(30, $content['SaasInstance']['numberOfIncludedUsers']);
-        }
     }
 }
