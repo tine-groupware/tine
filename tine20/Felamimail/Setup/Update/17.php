9c3760ca4b14114e4a7a610bedfb8d8ccd652dbb
ccheng-dev <c.cheng@metaways.de>
2023-11-28 06:28:57 +0100

    feature(Felamimail/js): message display format depends on content type

--- tine20/Felamimail/Setup/Update/17.php
+++ tine20/Felamimail/Setup/Update/17.php
@@ -14,8 +14,15 @@
 class Felamimail_Setup_Update_17 extends Setup_Update_Abstract
 {
     const RELEASE017_UPDATE000 = __CLASS__ . '::update000';
+    const RELEASE017_UPDATE001 = __CLASS__ . '::update001';
 
     static protected $_allUpdates = [
+        self::PRIO_NORMAL_APP_STRUCTURE => [
+            self::RELEASE017_UPDATE001          => [
+                self::CLASS_CONST                   => self::class,
+                self::FUNCTION_CONST                => 'update001',
+            ],
+        ],
         self::PRIO_NORMAL_APP_UPDATE        => [
             self::RELEASE017_UPDATE000          => [
                 self::CLASS_CONST                   => self::class,
@@ -27,5 +34,17 @@ class Felamimail_Setup_Update_17 extends Setup_Update_Abstract
     public function update000()
     {
         $this->addApplicationUpdate(Felamimail_Config::APP_NAME, '17.0', self::RELEASE017_UPDATE000);
+    }
+    
+    public function update001()
+    {
+        Tinebase_TransactionManager::getInstance()->rollBack();
+
+        if ($this->getTableVersion('felamimail_account') < 30) {
+            $this->setTableVersion('felamimail_account', 30);
+        }
+        $this->_backend->dropCol('felamimail_account', 'preserve_format');
+        
+        $this->addApplicationUpdate(Felamimail_Config::APP_NAME, '17.1', self::RELEASE017_UPDATE001);
     }
 }
