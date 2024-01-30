757c2087e78f5706ec8c2460e48db23126a6251e
Cornelius Weiß <c.weiss@metaways.de>
2024-01-30 18:44:45 +0100

    feature(Sales): Division Grants UI

--- tine20/Sales/Model/DivisionGrants.php
+++ tine20/Sales/Model/DivisionGrants.php
@@ -19,19 +19,19 @@ class Sales_Model_DivisionGrants extends Tinebase_Model_Grants
 {
     public const MODEL_NAME_PART    = 'DivisionGrants';
 
-    public const GRANT_ADMIN_DEBITOR = self::GRANT_ADMIN . '_' . Sales_Model_Debitor::MODEL_NAME_PART;
+    public const GRANT_ADMIN_DEBITOR = Sales_Model_Debitor::MODEL_NAME_PART . '_' . self::GRANT_ADMIN;
 
-    public const GRANT_ADMIN_DOCUMENT_DELIVERY = self::GRANT_ADMIN . '_' . Sales_Model_Document_Delivery::MODEL_NAME_PART;
-    public const GRANT_ADMIN_DOCUMENT_INVOICE = self::GRANT_ADMIN . '_' . Sales_Model_Document_Invoice::MODEL_NAME_PART;
-    public const GRANT_ADMIN_DOCUMENT_OFFER = self::GRANT_ADMIN . '_' . Sales_Model_Document_Offer::MODEL_NAME_PART;
-    public const GRANT_ADMIN_DOCUMENT_ORDER = self::GRANT_ADMIN . '_' . Sales_Model_Document_Order::MODEL_NAME_PART;
+    public const GRANT_ADMIN_DOCUMENT_DELIVERY = Sales_Model_Document_Delivery::MODEL_NAME_PART . '_' . self::GRANT_ADMIN;
+    public const GRANT_ADMIN_DOCUMENT_INVOICE = Sales_Model_Document_Invoice::MODEL_NAME_PART . '_' . self::GRANT_ADMIN;
+    public const GRANT_ADMIN_DOCUMENT_OFFER = Sales_Model_Document_Offer::MODEL_NAME_PART . '_' . self::GRANT_ADMIN;
+    public const GRANT_ADMIN_DOCUMENT_ORDER = Sales_Model_Document_Order::MODEL_NAME_PART . '_' . self::GRANT_ADMIN;
 
-    public const GRANT_READ_DEBITOR = self::GRANT_READ . '_' . Sales_Model_Debitor::MODEL_NAME_PART;
+    public const GRANT_READ_DEBITOR = Sales_Model_Debitor::MODEL_NAME_PART . '_' . self::GRANT_READ;
 
-    public const GRANT_READ_DOCUMENT_DELIVERY = self::GRANT_READ . '_' . Sales_Model_Document_Delivery::MODEL_NAME_PART;
-    public const GRANT_READ_DOCUMENT_INVOICE = self::GRANT_READ . '_' . Sales_Model_Document_Invoice::MODEL_NAME_PART;
-    public const GRANT_READ_DOCUMENT_OFFER = self::GRANT_READ . '_' . Sales_Model_Document_Offer::MODEL_NAME_PART;
-    public const GRANT_READ_DOCUMENT_ORDER = self::GRANT_READ . '_' . Sales_Model_Document_Order::MODEL_NAME_PART;
+    public const GRANT_READ_DOCUMENT_DELIVERY = Sales_Model_Document_Delivery::MODEL_NAME_PART . '_' . self::GRANT_READ;
+    public const GRANT_READ_DOCUMENT_INVOICE = Sales_Model_Document_Invoice::MODEL_NAME_PART . '_' . self::GRANT_READ;
+    public const GRANT_READ_DOCUMENT_OFFER = Sales_Model_Document_Offer::MODEL_NAME_PART . '_' . self::GRANT_READ;
+    public const GRANT_READ_DOCUMENT_ORDER = Sales_Model_Document_Order::MODEL_NAME_PART . '_' . self::GRANT_READ;
 
 
     /**
@@ -49,17 +49,17 @@ class Sales_Model_DivisionGrants extends Tinebase_Model_Grants
     public static function getAllGrants()
     {
         return [
-            self::GRANT_ADMIN,
-            self::GRANT_ADMIN_DEBITOR,
-            self::GRANT_ADMIN_DOCUMENT_DELIVERY,
-            self::GRANT_ADMIN_DOCUMENT_INVOICE,
-            self::GRANT_ADMIN_DOCUMENT_OFFER,
-            self::GRANT_ADMIN_DOCUMENT_ORDER,
             self::GRANT_READ_DEBITOR,
-            self::GRANT_READ_DOCUMENT_DELIVERY,
-            self::GRANT_READ_DOCUMENT_INVOICE,
+            self::GRANT_ADMIN_DEBITOR,
             self::GRANT_READ_DOCUMENT_OFFER,
+            self::GRANT_ADMIN_DOCUMENT_OFFER,
             self::GRANT_READ_DOCUMENT_ORDER,
+            self::GRANT_ADMIN_DOCUMENT_ORDER,
+            self::GRANT_READ_DOCUMENT_DELIVERY,
+            self::GRANT_ADMIN_DOCUMENT_DELIVERY,
+            self::GRANT_READ_DOCUMENT_INVOICE,
+            self::GRANT_ADMIN_DOCUMENT_INVOICE,
+            self::GRANT_ADMIN,
         ];
     }
 
@@ -75,49 +75,49 @@ class Sales_Model_DivisionGrants extends Tinebase_Model_Grants
     public static function getAllGrantsMC(): array
     {
         return [
-            self::GRANT_ADMIN => [
-                self::LABEL         => 'Admin', // _('Admin')
-                self::DESCRIPTION   => 'The grant to administrate this division (implies all other grants and the grant to set grants as well).', // _('The grant to administrate this division (implies all other grants and the grant to set grants as well).')
+            self::GRANT_READ_DEBITOR => [
+                self::LABEL         => 'Read Debitors', // _('Read Debitors')
+                self::DESCRIPTION   => 'The grant to read the division\'s debitors.', // _('The grant to read the division\'s debitors.')
             ],
             self::GRANT_ADMIN_DEBITOR => [
                 self::LABEL         => 'Admin Debitors', // _('Admin Debitors')
                 self::DESCRIPTION   => 'The grant to administrate the division\'s debitors.', // _('The grant to administrate the division\'s debitors.')
             ],
-            self::GRANT_ADMIN_DOCUMENT_DELIVERY => [
-                self::LABEL         => 'Admin Delivery Documents', // _('Admin Delivery Documents')
-                self::DESCRIPTION   => 'The grant to administrate the division\'s delivery documents.', // _('The grant to administrate the division\'s delivery documents.')
-            ],
-            self::GRANT_ADMIN_DOCUMENT_INVOICE => [
-                self::LABEL         => 'Admin Invoice Documents', // _('Admin Invoice Documents')
-                self::DESCRIPTION   => 'The grant to administrate the division\'s invoice documents.', // _('The grant to administrate the division\'s delivery documents.')
-            ],
-            self::GRANT_ADMIN_DOCUMENT_OFFER => [
-                self::LABEL         => 'Admin Offer Documents', // _('Admin Offer Documents')
-                self::DESCRIPTION   => 'The grant to administrate the division\'s offer documents.', // _('The grant to administrate the division\'s delivery documents.')
-            ],
-            self::GRANT_ADMIN_DOCUMENT_ORDER => [
-                self::LABEL         => 'Admin Order Documents', // _('Admin Order Documents')
-                self::DESCRIPTION   => 'The grant to administrate the division\'s order documents.', // _('The grant to administrate the division\'s delivery documents.')
-            ],
-            self::GRANT_READ_DEBITOR => [
-                self::LABEL         => 'Read Debitors', // _('Read Debitors')
-                self::DESCRIPTION   => 'The grant to read the division\'s debitors.', // _('The grant to read the division\'s debitors.')
-            ],
-            self::GRANT_READ_DOCUMENT_DELIVERY => [
-                self::LABEL         => 'Read Delivery Documents', // _('Read Delivery Documents')
-                self::DESCRIPTION   => 'The grant to read the division\'s delivery documents.', // _('The grant to read the division\'s delivery documents.')
-            ],
-            self::GRANT_READ_DOCUMENT_INVOICE => [
-                self::LABEL         => 'Read Invoice Documents', // _('Read Invoice Documents')
-                self::DESCRIPTION   => 'The grant to read the division\'s invoice documents.', // _('The grant to read the division\'s delivery documents.')
-            ],
             self::GRANT_READ_DOCUMENT_OFFER => [
-                self::LABEL         => 'Read Offer Documents', // _('Read Offer Documents')
+                self::LABEL         => 'Read Offers', // _('Read Offers')
                 self::DESCRIPTION   => 'The grant to read the division\'s offer documents.', // _('The grant to read the division\'s delivery documents.')
             ],
+            self::GRANT_ADMIN_DOCUMENT_OFFER => [
+                self::LABEL         => 'Admin Offers', // _('Admin Offers')
+                self::DESCRIPTION   => 'The grant to administrate the division\'s offer documents.', // _('The grant to administrate the division\'s delivery documents.')
+            ],
             self::GRANT_READ_DOCUMENT_ORDER => [
-                self::LABEL         => 'Read Order Documents', // _('Read Order Documents')
+                self::LABEL         => 'Read Orders', // _('Read Orders')
                 self::DESCRIPTION   => 'The grant to read the division\'s order documents.', // _('The grant to read the division\'s delivery documents.')
+            ],
+            self::GRANT_ADMIN_DOCUMENT_ORDER => [
+                self::LABEL         => 'Admin Orders', // _('Admin Orders')
+                self::DESCRIPTION   => 'The grant to administrate the division\'s order documents.', // _('The grant to administrate the division\'s delivery documents.')
+            ],
+            self::GRANT_READ_DOCUMENT_DELIVERY => [
+                self::LABEL         => 'Read Deliveries', // _('Read Deliveries')
+                self::DESCRIPTION   => 'The grant to read the division\'s delivery documents.', // _('The grant to read the division\'s delivery documents.')
+            ],
+            self::GRANT_ADMIN_DOCUMENT_DELIVERY => [
+                self::LABEL         => 'Admin Deliveries', // _('Admin Deliveries')
+                self::DESCRIPTION   => 'The grant to administrate the division\'s delivery documents.', // _('The grant to administrate the division\'s delivery documents.')
+            ],
+            self::GRANT_READ_DOCUMENT_INVOICE => [
+                self::LABEL         => 'Read Invoices', // _('Read Invoices')
+                self::DESCRIPTION   => 'The grant to read the division\'s invoice documents.', // _('The grant to read the division\'s delivery documents.')
+            ],
+            self::GRANT_ADMIN_DOCUMENT_INVOICE => [
+                self::LABEL         => 'Admin Invoices', // _('Admin Invoices')
+                self::DESCRIPTION   => 'The grant to administrate the division\'s invoice documents.', // _('The grant to administrate the division\'s delivery documents.')
+            ],
+            self::GRANT_ADMIN => [
+                self::LABEL         => 'Admin', // _('Admin')
+                self::DESCRIPTION   => 'The grant to administrate this division (implies all other grants and the grant to set grants as well).', // _('The grant to administrate this division (implies all other grants and the grant to set grants as well).')
             ],
         ];
     }
