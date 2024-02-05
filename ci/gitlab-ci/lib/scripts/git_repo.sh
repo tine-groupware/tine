489304d59da2287aab94cb399150b316044564d4
Milan Mertens <m.mertens@metaways.de>
2024-02-05 13:59:16 +0100

    ci(merge): git clone cache

--- ci/gitlab-ci/lib/scripts/git_repo.sh
+++ ci/gitlab-ci/lib/scripts/git_repo.sh
@@ -1,3 +1,38 @@
 git_repo_clone () {
     git clone ${CI_REPOSITORY_URL} --branch ${CI_COMMIT_REF_NAME} --depth 1 --single-branch ${CI_BUILDS_DIR}/${CI_PROJECT_NAMESPACE}/tine20
 }
+
+git_repo_clone_cached () {
+    set -x
+    targetDir=${1:-$CI_PROJECT_DIR}
+
+    cacheRepoPath=${CI_CUSTOM_CACHE_DIR}/git-cache/v1/$(echo "${CI_REPOSITORY_URL}" | cut -d '@' -f2)
+
+    mkdir -p $cacheRepoPath
+
+    pushd $cacheRepoPath
+    if ! git rev-parse --is-inside-work-tree 2> /dev/null; then
+        log "not a git repo. Initialzing new bare git repo ..."
+        git init --bare
+    fi
+
+    log "setting up remote"
+    if git remote | grep origin > /dev/null; then
+        git remote rm origin
+    fi
+
+    git remote add origin "${CI_REPOSITORY_URL}"
+
+    log "fetching from gitlab" 
+    git fetch --force origin '*:*'
+    popd
+
+
+    log "cloning into working dir"
+    git clone $cacheRepoPath --branch "${CI_COMMIT_REF_NAME}" --local --no-hardlinks "${targetDir}"
+    pushd ${targetDir}
+    git remote rm origin
+    git remote add origin "${CI_REPOSITORY_URL}"
+    git fetch
+    popd
+}
