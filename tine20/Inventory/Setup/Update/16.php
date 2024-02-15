39bcedf3f924487019e1c93481529641825f1ff2
Jan Evers <j.evers@metaways.de>
2024-02-15 09:28:06 +0000

    fix(Inventory): Remove unique constraint to allow deleting multiple items

--- tine20/Inventory/Setup/Update/16.php
+++ tine20/Inventory/Setup/Update/16.php
@@ -14,6 +14,7 @@
 class Inventory_Setup_Update_16 extends Setup_Update_Abstract
 {
     const RELEASE016_UPDATE000 = __CLASS__ . '::update000';
+    const RELEASE016_UPDATE001 = __CLASS__ . '::update001';
 
     static protected $_allUpdates = [
         self::PRIO_NORMAL_APP_UPDATE        => [
@@ -22,10 +23,22 @@ class Inventory_Setup_Update_16 extends Setup_Update_Abstract
                 self::FUNCTION_CONST                => 'update000',
             ],
         ],
+        self::PRIO_NORMAL_APP_STRUCTURE     => [
+            self::RELEASE016_UPDATE001          => [
+                self::CLASS_CONST                   => self::class,
+                self::FUNCTION_CONST                => 'update001',
+            ],
+        ],
     ];
 
     public function update000()
     {
         $this->addApplicationUpdate('Inventory', '16.0', self::RELEASE016_UPDATE000);
+    }
+
+    public function update001()
+    {
+        Setup_SchemaTool::updateSchema( [ Inventory_Model_InventoryItem::class ] );
+        $this->addApplicationUpdate('Inventory', '16.1', self::RELEASE016_UPDATE001);
     }
 }
