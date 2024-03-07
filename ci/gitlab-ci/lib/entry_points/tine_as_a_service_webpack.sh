4f61c462ade43c0b5ec649328ffd51947b56a1ed
Milan Mertens <m.mertens@metaways.de>
2024-03-07 09:54:43 +0100

    ci(e2etest): entrypoint use bash

--- ci/gitlab-ci/lib/entry_points/tine_as_a_service_webpack.sh
+++ ci/gitlab-ci/lib/entry_points/tine_as_a_service_webpack.sh
@@ -1,9 +1,7 @@
-#!/bin/sh
-apk add git;
-
+#!/bin/bash
 echo -n 'wait for signal_files_ready ...'; while [ ! -f ${CI_BUILDS_DIR}/${CI_PROJECT_NAMESPACE}/tine20/signal_files_ready ]; do sleep 1; done; echo ' done'
 
-test_npm_install --ignore-scripts;
+test_npm_install --ignore-scripts
 touch ${CI_BUILDS_DIR}/${CI_PROJECT_NAMESPACE}/tine20/signal_js_deps_installed
 
 echo -n 'wait for signal_php_deps_installed ...'; while [ ! -f ${CI_BUILDS_DIR}/${CI_PROJECT_NAMESPACE}/tine20/signal_php_deps_installed ]; do sleep 1; done; echo ' done'
