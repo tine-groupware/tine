c51e191af3f116fd1fd552a20cb2d18c0990de4f
Paul Mehrer <p.mehrer@metaways.de>
2024-03-09 17:19:27 +0100

    tweak(TB) clean up Record Generic

--- tine20/Tinebase/Record/Generic.php
+++ tine20/Tinebase/Record/Generic.php
@@ -5,7 +5,7 @@
  * @package     Tinebase
  * @subpackage  Record
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
- * @copyright   Copyright (c) 2017-2017 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2017-2024 Metaways Infosystems GmbH (http://www.metaways.de)
  * @author      Paul Mehrer <p.mehrer@metaways.de>
  */
 
@@ -18,9 +18,4 @@
 class Tinebase_Record_Generic extends Tinebase_Record_Abstract
 {
     protected $_identifier = 'id';
-
-    public function setValidators(array $validators)
-    {
-        $this->_validators = $validators;
-    }
 }
