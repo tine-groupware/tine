85457c4778510fcff5f305bd359b711493c072c6
Cornelius Weiß <c.weiss@metaways.de>
2024-04-17 16:47:27 +0200

    fix(Sales): allow to update product free item positions

--- tine20/Sales/js/DocumentPosition/AbstractEditDialog.js
+++ tine20/Sales/js/DocumentPosition/AbstractEditDialog.js
@@ -30,6 +30,7 @@ Tine.Sales.AbstractEditDialog = Ext.extend(Tine.widgets.dialog.EditDialog, {
         const type = this.record.get('type');
         const isProductType = this.record.isProductType();
         const productId = this.record.get('product_id');
+        const isEmptyProduct = isProductType && !productId && !this.record.get('title') && !this.record.get('description');
         
         this.getForm().findField('product_id').allowBlank = !isProductType;
         this.getForm().items.items.forEach((field) => {
@@ -45,7 +46,7 @@ Tine.Sales.AbstractEditDialog = Ext.extend(Tine.widgets.dialog.EditDialog, {
 
             // disable fields unless product is chosen
             field.setDisabled(this.fixedFields.get(field.name) ||
-                (isProductType && !productId && ['type', 'product_id'].indexOf(field.name) < 0));
+                (isEmptyProduct && ['type', 'product_id'].indexOf(field.name) < 0));
         });
         if (isProductType) {
             this.record.computePrice();
