52dbfe76398ca466f350444041b37685db27bec9
Sohan Deshar <s.deshar@metaways.de>
2024-05-03 08:00:33 +0000

    VuePasswordChangeDialog

---
+++ tine20/Tinebase/js/widgets/dialog/vue/PasswordChangeDialog/PasswordChangeDialog.vue
@@ -0,0 +1,69 @@
+<!--
+/*
+ * Tine 2.0
+ *
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Sohan Deshar <sdeshar@metaways.de>
+ * @copyright   Copyright (c) 2024 Metaways Infosystems GmbH (http://www.metaways.de)
+ */
+-->
+<template>
+  <BForm class="mb-4">
+    <p v-if="dialogText !== null" class="mb-4">{{ dialogText }}</p>
+    <PasswordField
+      id="oldPassword"
+      name="oldPassword"
+      :label="String.format(window.i18n._('Old {0}'), passwordLabel)"
+      :clipboard="false"
+      ref="oldPasswordField"
+    />
+    <PasswordField
+      id="newPassword"
+      name="newPassword"
+      :label="String.format(window.i18n._('New {0}'), passwordLabel)"
+      ref="newPasswordField"
+    />
+    <PasswordField
+      id="newPasswordSecondTime"
+      name="newPasswordSecondTime"
+      :label="String.format(window.i18n._('Repeat New {0}'), passwordLabel)"
+      ref="newPasswordSecondTimeField"
+    />
+  </BForm>
+</template>
+
+<script setup>
+import PasswordField from '../components/PasswordField.vue'
+import { ref, computed } from 'vue'
+
+// eslint-disable-next-line
+const props = defineProps({
+  passwordLabel: { type: String, default: 'Password' },
+  dialogText: { type: String, default: null }
+})
+
+const oldPasswordField = ref()
+const newPasswordField = ref()
+const newPasswordSecondTimeField = ref()
+
+const getValue = () => {
+  return {
+    oldPassword: oldPasswordField.value.getValue(),
+    newPassword: newPasswordField.value.getValue(),
+    newPasswordSecondTime: newPasswordSecondTimeField.value.getValue()
+  }
+}
+
+const initialFocus = computed(() => oldPasswordField.value.$inputEl)
+
+defineExpose({ getValue, initialFocus }
+)
+
+// onBeforeMount(() => {
+//   console.log(props.dialogText)
+// })
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ tine20/Tinebase/js/widgets/dialog/vue/PasswordDialog/PasswordDialog.vue
@@ -0,0 +1,76 @@
+<!--
+/*
+ * Tine 2.0
+ *
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Sohan Deshar <sdeshar@metaways.de>
+ * @copyright   Copyright (c) 2024 Metaways Infosystems GmbH (http://www.metaways.de)
+ */
+-->
+<template>
+  <div>
+    <p v-if="questionText">{{ questionText }}</p>
+    <PasswordField
+      ref="passwordFieldRef"
+      @paste="onPaste"
+      @keydown="onKeydown"
+      @keypress="onKeypress"
+      :clipboard="clipboard"
+      :locked="locked"
+      :label="passwordLabel"
+    />
+    <BButton
+      v-if="hasPwGen"
+      @click="onPWGenClick"
+      variant="outline-primary"
+      class="mb-2"
+    >{{ window.i18n._('Generate password') }}</BButton>
+  </div>
+
+</template>
+
+<script setup>
+import PasswordField from '../components/PasswordField.vue'
+import { ref, inject, computed } from 'vue'
+
+const props = defineProps({
+  passwordLabel: { type: String, default: 'Password' },
+  questionText: { type: String, default: null },
+  hasPwGen: { type: Boolean, default: false },
+  allowBlank: { type: Boolean, default: false },
+  locked: { type: Boolean, default: true },
+  clipboard: { type: Boolean, default: true },
+  pwMandatoryByPolicy: { type: Boolean, default: true },
+
+  injectKey: { type: String }
+})
+
+const { genPW, eventBus } = inject(props.injectKey)
+
+const onPWGenClick = function () {
+  const p = genPW()
+  passwordFieldRef.value.setValue(p)
+}
+
+const onPaste = () => {}
+const onKeydown = () => {}
+const onKeypress = (e) => {
+  if (!disableok.value && e.key === 'Enter') {
+    eventBus.emit('apply', passwordFieldRef.value.getValue())
+  }
+}
+
+const passwordFieldRef = ref()
+
+const getValue = () => passwordFieldRef.value?.getValue() || ''
+
+const disableok = computed(() => props.allowBlank ? false : passwordFieldRef.value?.getValue().length === 0)
+
+const initialFocus = computed(() => passwordFieldRef.value.$inputEl)
+
+defineExpose({ getValue, disableok, initialFocus })
+</script>
+
+<style scoped>
+
+</style>
