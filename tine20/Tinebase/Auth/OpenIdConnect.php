93063ae01ddec023c212f4727be2cf7d5b2f1f85
Christian Feitl <c.feitl@metwaways.net>
2024-09-10 10:21:11 +0200

    fix(Tinebase): error with SSO login

--- tine20/Tinebase/Auth/OpenIdConnect.php
+++ tine20/Tinebase/Auth/OpenIdConnect.php
@@ -90,7 +90,7 @@ class Tinebase_Auth_OpenIdConnect extends Tinebase_Auth_Adapter_Abstract
             . ' Set provider redirect url: ' . $redirectUrl);
         $oidc->setRedirectURL($redirectUrl);
 
-        $oidc->addScope('openid email');
+        $oidc->addScope(['openid','email']);
         $oidc->setResponseTypes(array('code'));
         $oidc->setAllowImplicitFlow(true);
 
