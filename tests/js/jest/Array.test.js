2609d9e4d6f60ccdcb8f24c072893bdeb551ba96
Cornelius Weiß <c.weiss@metaways.de>
2024-09-25 13:24:30 +0200

    feature(Tinebase) js unittests with jest

---
+++ tests/js/jest/Array.test.js
@@ -0,0 +1,14 @@
+const sum = require('ux/Array.js');
+
+const testArr = ["green", "red", "blue", "red"];
+
+test('Array.diff', () => {
+    expect(testArr.diff(["green", "yellow", "red"]).length).toBe(1);
+    expect(testArr.diff(["green"], "yellow", "red", "blue").length).toBe(0);
+    expect(testArr.diff(["green", "yellow", "red"])).toStrictEqual(['blue']);
+});
+
+test('Array.intersect', () => {
+    expect(testArr.intersect(["blue", "green"]).length).toBe(2);
+    expect(testArr.intersect(["blue", "green"])).toStrictEqual(['green', 'blue']);
+});
