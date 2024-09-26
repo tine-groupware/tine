db4f6cad32f7523e7de0a24c60876af2781f43af
Paul Mehrer <p.mehrer@metaways.de>
2024-09-26 14:45:14 +0200

    tweak(Sales) fire document deleted event

--- tine20/Sales/Controller/Document/Abstract.php
+++ tine20/Sales/Controller/Document/Abstract.php
@@ -364,6 +364,8 @@ abstract class Sales_Controller_Document_Abstract extends Tinebase_Controller_Re
             Tinebase_Record_Expander::expandRecord($precursorDocument->{Tinebase_Model_DynamicRecordWrapper::FLD_RECORD});
             $precursorDocument->{Tinebase_Model_DynamicRecordWrapper::FLD_RECORD}->updateFollowupStati(true);
         }
+
+        Tinebase_Event::fireEvent(new Sales_Event_DocumentDeleted($record));
     }
 
     /**
