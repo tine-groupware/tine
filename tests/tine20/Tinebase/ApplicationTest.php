69a4c1442d3a998a8b26e60d92f3e9d403cc1118
Stefanie Stamer <s.stamer@metaways.de>
2024-09-27 18:08:14 +0200

    fix test

--- tests/tine20/Tinebase/ApplicationTest.php
+++ tests/tine20/Tinebase/ApplicationTest.php
@@ -306,6 +306,7 @@ class Tinebase_ApplicationTest extends TestCase
                 Addressbook_Model_ListRole::class,
                 Addressbook_Model_Salutation::class,
                 Addressbook_Model_ContactGrants::class,
+                Addressbook_Model_Sites::class
             ),
             'Admin' => array(
                 Admin_Model_Config::class,
