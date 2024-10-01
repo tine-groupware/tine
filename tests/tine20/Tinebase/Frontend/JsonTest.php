b5ef0b3e8eebbf1b18a609034327ee042b7aa18e
ccheng-dev <c.cheng@metaways.de>
2024-10-01 11:44:33 +0200

    fix(test/Tinebase): assert larger registry data size

--- tests/tine20/Tinebase/Frontend/JsonTest.php
+++ tests/tine20/Tinebase/Frontend/JsonTest.php
@@ -737,7 +737,7 @@ class Tinebase_Frontend_JsonTest extends TestCase
         self::assertArrayHasKey('Sales.createPaperSlip', $registryData['Tinebase']['serviceMap']['services']);
         self::assertSame(60, $registryData['Tinebase']['serviceMap']['services']['Sales.createPaperSlip']['apiTimeout']);
         
-        self::assertLessThan(2600000, strlen(json_encode($registryData)), 'registry size got too big');
+        self::assertLessThan(2700000, strlen(json_encode($registryData)), 'registry size got too big');
     }
 
     protected function _assertImportExportDefinitions($registryData)
