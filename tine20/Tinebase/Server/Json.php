34a7edc34e8ae1422044ebfcf0df74bb9644c013
Paul Mehrer <p.mehrer@metaways.de>
2025-01-06 18:23:13 +0100

    tweak(TB Login) enable username less login

--- tine20/Tinebase/Server/Json.php
+++ tine20/Tinebase/Server/Json.php
@@ -574,7 +574,8 @@ class Tinebase_Server_Json extends Tinebase_Server_Abstract implements Tinebase_
             'Tinebase.setLocale',
             'Tinebase.checkAuthToken',
             'Tinebase_AreaLock.unlock',
-            'Tinebase.getWebAuthnAuthenticateOptionsForMFA'
+            'Tinebase.getWebAuthnAuthenticateOptionsForMFA',
+            'Tinebase.getWebAuthnAuthenticateOptionsForLogin',
         );
 
         // check json key for all methods but some exceptions
