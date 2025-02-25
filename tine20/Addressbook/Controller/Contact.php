770f930159ef888fdf4a2442c24b2040a0c54104
Paul Mehrer <p.mehrer@metaways.de>
2025-02-25 16:55:30 +0100

    tweak(Adb Contact) contact group properties acl fix

--- tine20/Addressbook/Controller/Contact.php
+++ tine20/Addressbook/Controller/Contact.php
@@ -110,10 +110,12 @@ class Addressbook_Controller_Contact extends Tinebase_Controller_Record_Abstract
     {
         $contact = parent::get($_id, $_containerId, $_getRelatedData, $_getDeleted, $_aclProtect);
 
-        if($_id) {
+        if ($_id) {
             $listController = Addressbook_Controller_List::getInstance();
             $groups = $listController->getMemberships($_id);
-            $contact->groups = $listController->getBackend()->getMultiple($groups);
+            $contact->groups = $listController->search(Tinebase_Model_Filter_FilterGroup::getFilterForModel(Addressbook_Model_List::class, [
+                ['field' => 'id', 'operator' => 'in', 'value' => $groups]
+            ]));
             $contact->groups->members = null;
         }
 
