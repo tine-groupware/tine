8722b1ecc3e857fea0f06c0750f7735b38bafa58
Philipp Schüle <p.schuele@metaways.de>
2025-04-09 09:30:48 +0200

    tweak(Felamimail/Message): catch 403 in cleanupDrafts

--- tine20/Felamimail/Controller/Message.php
+++ tine20/Felamimail/Controller/Message.php
@@ -1626,16 +1626,15 @@ class Felamimail_Controller_Message extends Tinebase_Controller_Record_Abstract
                 if ($draftFolder) {
                     $draftFolderIds[] = $draftFolder->getId();
                 }
-            } catch (Exception $e) {
-                if (($e instanceof Felamimail_Exception_IMAPServiceUnavailable
-                        || $e instanceof Felamimail_Exception_IMAPInvalidCredentials)
-                ) {
-                    if (Tinebase_Core::isLogLevel(Zend_Log::INFO)) {
-                        Tinebase_Core::getLogger()->info(__METHOD__ . '::' . __LINE__ . ' ' . $e);
-                    }
-                } else {
-                    Tinebase_Exception::log($e);
+            } catch (Felamimail_Exception_IMAPServiceUnavailable
+                | Felamimail_Exception_IMAPInvalidCredentials
+                | Tinebase_Exception_AccessDenied $e
+            ) {
+                if (Tinebase_Core::isLogLevel(Zend_Log::INFO)) {
+                    Tinebase_Core::getLogger()->info(__METHOD__ . '::' . __LINE__ . ' ' . $e);
                 }
+            } catch (Exception $e) {
+                Tinebase_Exception::log($e);
             }
         }
 
