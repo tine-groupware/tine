42544126cd1c4f02c30b71a941c9d11fba28c3b3
Paul Mehrer <p.mehrer@metaways.de>
2025-08-08 17:37:32 +0200

    tweak(Setup) added cli importConfigToDb

---
+++ tests/setup/files/importConfigToDb.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+return [
+    SSO_Config::APP_NAME => [
+        SSO_Config::OAUTH2 => [
+            SSO_Config::OAUTH2_KEYS => [
+                0 => [
+                    'foo' => 'unittest',
+                    'use' => 'unittest',
+                ],
+            ],
+        ],
+    ],
+];
