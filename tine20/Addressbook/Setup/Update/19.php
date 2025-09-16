cc2a9a8126655091d63569d9ad7108071f5e48b4
Philipp Schüle <p.schuele@metaways.de>
2025-09-16 11:11:49 +0200

    conf(*/Update/19): update all apps to 19.0

---
+++ tine20/Addressbook/Setup/Update/19.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * tine Groupware
+ *
+ * @package     Addressbook
+ * @subpackage  Setup
+ * @license     https://www.gnu.org/licenses/agpl.html AGPL3
+ * @copyright   Copyright (c) 2025 Metaways Infosystems GmbH (https://www.metaways.de)
+ * @author      Philipp Schüle <p.schuele@metaways.de>
+ *
+ * this is 2026.11 (ONLY!)
+ */
+class Addressbook_Setup_Update_19 extends Setup_Update_Abstract
+{
+    protected const RELEASE019_UPDATE000 = __CLASS__ . '::update000';
+
+    static protected $_allUpdates = [
+        self::PRIO_NORMAL_APP_UPDATE        => [
+            self::RELEASE019_UPDATE000          => [
+                self::CLASS_CONST                   => self::class,
+                self::FUNCTION_CONST                => 'update000',
+            ],
+        ],
+    ];
+
+    public function update000(): void
+    {
+        $this->addApplicationUpdate(Addressbook_Config::APP_NAME, '19.0', self::RELEASE019_UPDATE000);
+    }
+}
