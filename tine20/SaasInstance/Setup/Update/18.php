014530cc6e4d688b1788b98e0677a59b091b7fdd
Philipp Schüle <p.schuele@metaways.de>
2025-10-01 15:55:13 +0200

    conf(SaasInstance/Update): update to 18.0

---
+++ tine20/SaasInstance/Setup/Update/18.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Tine 2.0
+ *
+ * @package     SaasInstance
+ * @subpackage  Setup
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL3
+ * @copyright   Copyright (c) 2025 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Philipp Schüle <p.schuele@metaways.de>
+ *
+ * this is 2025.11 (ONLY!)
+ */
+class SaasInstance_Setup_Update_18 extends Setup_Update_Abstract
+{
+    const RELEASE018_UPDATE000 = __CLASS__ . '::update000';
+
+    static protected $_allUpdates = [
+        self::PRIO_NORMAL_APP_UPDATE        => [
+            self::RELEASE018_UPDATE000          => [
+                self::CLASS_CONST                   => self::class,
+                self::FUNCTION_CONST                => 'update000',
+            ],
+        ],
+    ];
+
+    public function update000()
+    {
+        $this->addApplicationUpdate(SaasInstance_Config::APP_NAME, '18.0', self::RELEASE018_UPDATE000);
+    }
+}
