2cf3e0067e483d2efd536634f451797ac312c841
Cornelius Weiß <c.weiss@metaways.de>
2026-02-17 09:41:49 +0100

    fix(Sales): vat does not change if vat free supplier is selected after net or gross

--- tine20/Sales/js/Document/AbstractEditDialog.js
+++ tine20/Sales/js/Document/AbstractEditDialog.js
@@ -149,7 +149,7 @@ Tine.Sales.Document_AbstractEditDialog = Ext.extend(Tine.widgets.dialog.EditDial
                     }, 0))
                 } else {
                     if (!record.get('sales_tax_by_rate')?.length || record.get('sales_tax_by_rate').length === 1) {
-                        sales_tax_by_rate = record.get('sales_tax_by_rate')?.[0]?.tax_rate || defaultTaxRate
+                        sales_tax_by_rate = /*record.get('sales_tax_by_rate')?.[0]?.tax_rate ||*/ defaultTaxRate
                         sales_tax = record.get('sales_tax_by_rate')?.[0]?.tax_amount || 0
                         sales_tax = (record.get('gross_sum') || 0) - (record.get('gross_sum') || 0) / (1 + sales_tax_by_rate / 100)
                     } else {
@@ -169,7 +169,7 @@ Tine.Sales.Document_AbstractEditDialog = Ext.extend(Tine.widgets.dialog.EditDial
                     }, 0))
                 } else {
                     if (!record.get('sales_tax_by_rate')?.length || record.get('sales_tax_by_rate').length === 1) {
-                        sales_tax_by_rate = record.get('sales_tax_by_rate')?.[0]?.tax_rate || defaultTaxRate
+                        sales_tax_by_rate = /*record.get('sales_tax_by_rate')?.[0]?.tax_rate ||*/ defaultTaxRate
                         sales_tax = record.get('sales_tax_by_rate')?.[0]?.tax_amount || 0
                         sales_tax = (record.get('net_sum') || 0) / 100 * sales_tax_by_rate;
                     } else {
