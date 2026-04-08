d99a1bb860ff9bd2ef17cec26ddfb4bc9c54959b
Cornelius Weiß <c.weiss@metaways.de>
2026-04-08 16:46:33 +0000

    tweak(Sales): improve module order

--- tine20/Sales/js/Sales.js
+++ tine20/Sales/js/Sales.js
@@ -48,8 +48,10 @@ Tine.Sales.MainScreen = Ext.extend(Tine.widgets.MainScreen, {
     activeContentType: 'Product',
     contentTypes: [
         {modelName: 'Product', requiredRight: 'manage_products', singularContainerMode: true},
-        {modelName: 'Contract', requiredRight: 'manage_contracts', singularContainerMode: true, genericCtxActions: ['grants']},
         {modelName: 'Customer', requiredRight: 'manage_customers', singularContainerMode: true},
+        {modelName: 'Debitor', requiredRight: 'manage_customers', singularContainerMode: true},
+        {modelName: 'Contract', requiredRight: 'manage_contracts', singularContainerMode: true, genericCtxActions: ['grants']},
+
         // {modelName: 'Supplier', requiredRight: 'manage_suppliers', singularContainerMode: true},
         // {modelName: 'PurchaseInvoice', requiredRight: 'manage_purchase_invoices', singularContainerMode: true},
 
@@ -57,7 +59,7 @@ Tine.Sales.MainScreen = Ext.extend(Tine.widgets.MainScreen, {
         // TODO add migration to new documents
         // {modelName: 'Invoice', requiredRight: 'manage_invoices', singularContainerMode: true},
         // {modelName: 'OrderConfirmation', requiredRight: 'manage_orderconfirmations', singularContainerMode: true},
-        {modelName: 'Offer', requiredRight: 'manage_offers', singularContainerMode: true},
+        // {modelName: 'Offer', requiredRight: 'manage_offers', singularContainerMode: true},
 
         // new documents
         {modelName: 'Document_Offer', requiredRight: 'manage_offers', singularContainerMode: true},
@@ -66,11 +68,10 @@ Tine.Sales.MainScreen = Ext.extend(Tine.widgets.MainScreen, {
         {modelName: 'Document_Invoice', requiredRight: 'manage_invoices', singularContainerMode: true},
 
         // special/advanced lists
-        {modelName: 'Debitor', requiredRight: 'manage_customers', singularContainerMode: true, group: 'Lists'},
-        {modelName: 'DocumentPosition_Offer', requiredRight: 'manage_offers', singularContainerMode: true, group: 'Lists'},
-        {modelName: 'DocumentPosition_Order', requiredRight: 'manage_orderconfirmations', singularContainerMode: true, group: 'Lists'},
-        {modelName: 'DocumentPosition_Delivery', requiredRight: 'manage_orderconfirmations', singularContainerMode: true, group: 'Lists'},
-        {modelName: 'DocumentPosition_Invoice', requiredRight: 'manage_invoices', singularContainerMode: true, group: 'Lists'},
+        {modelName: 'DocumentPosition_Offer', requiredRight: 'manage_offers', singularContainerMode: true, group: 'Document lines'}, // _('Document lines')
+        {modelName: 'DocumentPosition_Order', requiredRight: 'manage_orderconfirmations', singularContainerMode: true, group: 'Document lines'},
+        {modelName: 'DocumentPosition_Delivery', requiredRight: 'manage_orderconfirmations', singularContainerMode: true, group: 'Document lines'},
+        {modelName: 'DocumentPosition_Invoice', requiredRight: 'manage_invoices', singularContainerMode: true, group: 'Document lines'},
     ]
 });
 
