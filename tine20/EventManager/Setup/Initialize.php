05bc23b2752e74605b34074194a04aea963c0112
Philipp Schüle <p.schuele@metaways.de>
2026-05-20 10:18:26 +0200

    tweak(EM/Initialize): import costcenter data from csv file

--- tine20/EventManager/Setup/Initialize.php
+++ tine20/EventManager/Setup/Initialize.php
@@ -83,6 +83,11 @@ class EventManager_Setup_Initialize extends Setup_Initialize
         Tinebase_Controller_EvaluationDimension::addModelsToDimension(Tinebase_Model_EvaluationDimension::COST_CENTER, [
             EventManager_Model_Event::class,
         ]);
+
+        $def = Tinebase_ImportExportDefinition::getInstance()->getByName('tinebase_import_costcenter_csv');
+        $importer = Tinebase_Import_Csv_Generic::createFromDefinition($def);
+        $importer->importFile(__DIR__ . '/DemoData/files/costcenter.csv');
+
         Tinebase_Controller_EvaluationDimension::addModelsToDimension(Tinebase_Model_EvaluationDimension::COST_BEARER, [
             EventManager_Model_Event::class,
         ]);
