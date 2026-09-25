e1fa5959a41d8d4a81f95017719dbdbc15e188d1
Tonia Wulff <t.wulff@metaways.de>
2026-09-25 11:28:33 +0200

    fix(EM): E-mail not required for minor

--- tine20/EventManager/js/EventEditDialog.js
+++ tine20/EventManager/js/EventEditDialog.js
@@ -35,7 +35,7 @@ const CONTACT_FIELDS_SHARED_CONFIG = {
         'tel_cell', 'tel_work', 'adr_one_street', 'adr_one_street2',
         'adr_one_postalcode', 'adr_one_locality', 'adr_one_region',
         'adr_one_countryname'],
-    defaultRequiredFields: ['n_given', 'n_family', 'email'],
+    defaultRequiredFields: ['n_given', 'n_family', 'bday'],
 };
 
 Tine.EventManager.EventEditDialog = Ext.extend(Tine.widgets.dialog.EditDialog, {
